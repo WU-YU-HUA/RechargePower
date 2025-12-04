a40fefe7d85c3b1ac1a265971efb0d6facff8c28
HarryWu <a0973350996@gmail.com>
2025-12-04 10:59:24 +0800

    User model initial & app setting & url setting

--- rechargepower/urls.py
+++ rechargepower/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls")),
+    path("api/order/", include("order.urls")),
+    path("api/gift/", include("gift.urls")),
 ]
--- user/models.py
+++ user/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        user = self.model(username=username, **extra_fields)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(username, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, unique=True, max_length=120)
+    password = models.CharField(null=True, blank=True, max_length=120)
+    name = models.TextField(blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True,null=True)
+
+    is_superuser = models.BooleanField(blank=True, null=False, default=False) #使用admin
+    is_staff = models.BooleanField(blank=True, null=False, default=False) #進入admin
+    is_active = models.BooleanField(blank=True, null=False, default=True) #可使用的帳號
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
